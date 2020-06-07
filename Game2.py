25ff4f5fc6add0dfb680bd723920b0ddeddcde21
vscala <57908042+vscala@users.noreply.github.com>
2020-06-07 00:35:15 -0400

    Starter code
    -

---
+++ Game2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+A text based shooter game (not really)
+"""
+
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+stats = {
+	"Health" : 0,
+	"Attack_Damage" : 0,
+	"Ranged_Damage" : 0,
+	"Stamina" : 0
+}
+
+chatter = "\
+###################\n\
+###################\n\
+###################"
+
+def start():
+	print(chatter)
+
+
+
+#Begin game	
+if __name__ == "__main__": start()

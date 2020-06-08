e262372ea9aa233eaa6116aee2a6b169e9df5bd7
Kathoey <nicholasnguyen75@gmail.com>
2020-06-08 17:15:25 -0400

    Made a character class that can move around the map

--- Game.py
+++ Game.py
@@ -16,27 +16,61 @@ stats = {
 
 map = [\
 ['#', '#', '#', '#'],\
-['#', ' ', ' ', '#'],\
-['#', ' ', ' ', '#'],\
+['#', '#', '#', '#'],\
+['#', '#', '#', '#'],\
 ['#', '#', '#', '#']\
 ]
+
+#character class for controllable character and npcs(?)
+class character:
+	def __init__(self, x, y, symbol):
+		self.x = x
+		self.y = y
+		self.symbol = symbol
+		map[x][y] = symbol
+
+	def moveUp(self):
+		if self.x - 1 >= 0:
+			map[self.x][self.y] = '#'
+			self.x = self.x - 1
+			map[self.x][self.y] = self.symbol
+
+	def moveLeft(self):
+		if self.y - 1 >= 0:
+			map[self.x][self.y] = '#'
+			self.y = self.y - 1 
+			map[self.x][self.y] = self.symbol
+
+	def moveRight(self):
+		if self.y + 1 < 4:
+			map[self.x][self.y] = '#'
+			self.y = self.y + 1
+			map[self.x][self.y] = self.symbol
+
+	def moveDown(self):
+		if self.x + 1 < 4:
+			map[self.x][self.y] = '#'
+			self.x = self.x + 1
+			map[self.x][self.y] = self.symbol		
+
 
 gameRunning = True
 
 #Print current map state
 def displayMap():
 	for row in map: print("".join(row))
+	print('\n')
 
 #Start Actions
-def moveW():
-	pass
-def moveA():
-	pass
-def moveS():
-	pass
-def moveD():
-	pass
-def stopGame():
+def moveW(character):
+	character.moveUp()
+def moveA(character):
+	character.moveLeft()
+def moveS(character):
+	character.moveDown()
+def moveD(character):
+	character.moveRight()
+def stopGame(character):
 	global gameRunning
 	gameRunning = False
 #End Actions
@@ -52,15 +86,17 @@ actions = {
 
 #Game starts here
 def start():
+	mainChar = character(0,0, "&")
 	displayMap()
-	read()
+	read(mainChar)
 
 #Read in player input
-def read():
+def read(character):
 	global gameRunning
 	while gameRunning:
 		input_char = str(msvcrt.getch())[2]
-		if input_char.upper() in actions: actions[input_char.upper()]()
+		if input_char.upper() in actions: actions[input_char.upper()](character)
+		displayMap()
 
 #Call start function
 if __name__ == "__main__": start()

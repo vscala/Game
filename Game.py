269dbba9d34cf1d137b12a1fd9ca60db47122990
vscala <57908042+vscala@users.noreply.github.com>
2020-06-16 01:04:13 -0400

    fix tabs

--- Game.py
+++ Game.py
@@ -12,41 +12,41 @@ __license__ = "MIT"
 
 
 def main():
-    screen_width = 80
-    screen_height = 50
+	screen_width = 80
+	screen_height = 50
 
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+	player_x = int(screen_width / 2)
+	player_y = int(screen_height / 2)
 
-    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+	libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'The Game that is Played', False)
+	libtcod.console_init_root(screen_width, screen_height, 'The Game that is Played', False)
   
-    key = libtcod.Key()
-    mouse = libtcod.Mouse()
+	key = libtcod.Key()
+	mouse = libtcod.Mouse()
 
-    while not libtcod.console_is_window_closed():
-        libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_flush()
+	while not libtcod.console_is_window_closed():
+		libtcod.console_set_default_foreground(0, libtcod.white)
+		libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+		libtcod.console_flush()
 
-        action = handle_keys(key)
+		action = handle_keys(key)
 
-        move = action.get('move')
-        exit = action.get('exit')
-        fullscreen = action.get('fullscreen')
+		move = action.get('move')
+		exit = action.get('exit')
+		fullscreen = action.get('fullscreen')
 
-        if move:
-            dx, dy = move
-            player_x += dx
-            player_y += dy
+		if move:
+			dx, dy = move
+			player_x += dx
+			player_y += dy
 
 		if exit:
-            return True
+			return True
 
-        if fullscreen:
-            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+		if fullscreen:
+			libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 
 if __name__ == '__main__':
-    main()
+	main()

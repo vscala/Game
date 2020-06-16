3a8f7be932ea7ac6f2c583c4d8e8fb7300481a3d
Kathoey <nicholasnguyen75@gmail.com>
2020-06-16 00:48:45 -0400

    added input handling and mouse/key variables along with playerx and playery vars

--- Game.py
+++ Game.py
@@ -15,19 +15,36 @@ def main():
     screen_width = 80
     screen_height = 50
 
+	player_x = int(screen_width / 2)
+	player_y = int(screen_width / 2)
+
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
     libtcod.console_init_root(screen_width, screen_height, 'The Game that is Played', False)
 
+	key = libtcod.Key()
+	mouse = libtcod.Mouse()
+
     while not libtcod.console_is_window_closed():
         libtcod.console_set_default_foreground(0, libtcod.white)
-        libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
         libtcod.console_flush()
 
-        key = libtcod.console_check_for_keypress()
+        action = handle_keys(key)
 
-        if key.vk == libtcod.KEY_ESCAPE:
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+			        if exit:
             return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
 
 
 if __name__ == '__main__':
